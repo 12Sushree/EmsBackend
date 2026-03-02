f7ebb93818615d0fb09d1a9b051d4e07ff888d96
12Sushree <sushreeta.shu@gmail.com>
2026-03-02 13:32:11 +0530

    Send Mail Bug Fixed!

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -6,9 +6,9 @@ dns.setDefaultResultOrder("ipv4first");
 exports.sendMail = async ({ to, subject, html }) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: Number(process.env.MAIL_PORT),
-      secure: process.env.MAIL_PORT == 465,
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
