fab3afa3b084411c8587b9418fee635ddbb3dcf3
12Sushree <sushreeta.shu@gmail.com>
2026-03-02 17:59:00 +0530

    Send Mail Bug Fixed!

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,25 +1,29 @@
-const brevo = require("@getbrevo/brevo");
+const axios = require("axios");
 
 exports.sendMail = async ({ to, subject, html }) => {
   try {
-    const client = brevo.ApiClient.instance;
-    client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
-
-    const apiInstance = new brevo.TransactionalEmailsApi();
-
-    await apiInstance.sendTransacEmail({
-      sender: {
-        name: "EMS System",
-        email: "sushreeta4112@gmail.com", // must be verified in Brevo
+    await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "EMS System",
+          email: "sushreeta4112@gmail.com", // must be verified in Brevo
+        },
+        to: [{ email: to }],
+        subject: subject,
+        htmlContent: html,
       },
-      to: [{ email: to }],
-      subject: subject,
-      htmlContent: html,
-    });
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "api-key": process.env.BREVO_API_KEY,
+        },
+      },
+    );
 
     console.log("Email sent successfully");
   } catch (error) {
-    console.error("Mail Error:", error.response?.body || error.message);
+    console.error("Mail Error:", error.response?.data || error.message);
     throw new Error("Email could not be sent");
   }
 };
